7aced79ab9876de2b5f70469c25aab0a4b2a8c51
julipp01 <julio_arpp@hotmail.com>
2025-03-11 21:15:21 -0500

    Usando DATABASE_URL en Railway

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,8 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306, // Asegurar el puerto correcto
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
-// Verificar la conexión
 pool.getConnection()
   .then(conn => {
     console.log('✅ Conectado a MySQL en Railway');
@@ -23,5 +13,7 @@ pool.getConnection()
   });
 
 module.exports = pool;
+
+
 
 
