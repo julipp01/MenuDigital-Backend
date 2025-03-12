d3e84059518bb74a7b296b0db76ee7a84f716cbb
julipp01 <julio_arpp@hotmail.com>
2025-03-11 19:29:31 -0500

    Fix bcrypt module

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 // backend/src/routes/auth.js
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 const pool = require("../config/db");
 
