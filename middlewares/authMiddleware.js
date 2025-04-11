d7671881e2de6a7ff7622c7deb4152272310a826
julipp01 <julio_arpp@hotmail.com>
2025-04-11 00:09:21 -0500

    Fix encoding issue in authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   const token = authHeader.split(" ")[1];
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.status(401).json({ error: "Token inv·lido o expirado" });
+    if (err) return res.status(401).json({ error: "Token inv√°lido o expirado" });
 
     // Verificar que el token contenga restaurantId
     if (!decoded.restaurantId) {
